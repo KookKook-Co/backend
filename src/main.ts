8b22cf8a008413efa26f27ae66ec18e45ecb19e7
Anapat Jiamwijitkul <pat_sk135@hotmail.com>
2020-03-12 20:16:02 +0700

    fix(): enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, { cors: true });
+
     await app.listen(process.env.PORT, () => {
         // console.log(`Listening to localhost:${process.env.PORT}`);
     });
